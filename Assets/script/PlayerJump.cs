4fff381a115e790388bf657c359447c865cfde91
IzanMCR <129779787+IzanMCR@users.noreply.github.com>
2023-12-16 15:13:09 +0000

    Add files via upload
    Changes in PlayersJump and State Machine

---
+++ Assets/script/PlayerJump.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using static UnityEngine.UI.Image;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] float JumpForce;
+    [SerializeField] float lineLength;
+    [SerializeField] float offset;
+    [SerializeField] bool OnGround;
+    [SerializeField] bool DoubleJump;
+    public bool CanDoubleJump;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - offset);
+        Vector2 target = new Vector2(transform.position.x, transform.position.y - offset - lineLength);
+        Debug.DrawLine(origin, target, Color.black);
+        OnGround = Physics2D.Raycast(origin, Vector2.down, lineLength);
+        if (OnGround)
+        {
+            DoubleJump = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (OnGround)
+            {
+                Jump();
+                Debug.Log("Jump");
+            }
+            else if (CanDoubleJump && !DoubleJump)
+            {
+                Jump();
+                DoubleJump = true;
+                Debug.Log("JumpDouble");
+            }
+        }
+        void Jump()
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpForce);
+        }
+
+    }
+}
